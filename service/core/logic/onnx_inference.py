25287bfd70cc5d780d61033132572ddac7078f79
rakibulhaque9954 <rakibulhaque9954@gmail.com>
2023-09-21 00:19:42 +0900

    add haarcascade_frontalface_default.xml and update onnx_inference.py

--- service/core/logic/onnx_inference.py
+++ service/core/logic/onnx_inference.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import onnxruntime as rt
 import time
 import service.main as main
 
@@ -8,22 +7,36 @@ import service.main as main
 def emotions_detector(img_array):
     time_init = time.time()  # time started for calculating inference time
 
-    # Converting image to np array and resizing for the model/preprocessing
+    # Load the Haar Cascade classifier for face detection
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Converting image to np array
     image = img_array
-    image = cv2.resize(image, (256, 256))
-    im = np.float32(image)
-    im = np.expand_dims(im, axis=0)
+
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    emotions = []
+
+    for (x, y, w, h) in faces:
+        # Crop and resize the face region for the model/preprocessing
+        face = image[y:y + h, x:x + w]
+        face = cv2.resize(face, (256, 256))
+        im = np.float32(face)
+        im = np.expand_dims(im, axis=0)
+
+        # Inference by passing the face to the model
+        onnx_pred = main.model.run(['dense'], {'input_image': im})
+
+        # Predict emotion
+        class_names = ['angry', 'happy', 'neutral', 'sad', 'surprised']
+        emotion = class_names[np.argmax(onnx_pred[0][0])]
+        emotions.append(emotion)
+
     time_elapsed_preprocessing = f'{time.time() - time_init:.4f} secs'
-
-    # Inference by passing the image to the model
-    onnx_pred = main.model.run(['dense'], {'input_image': im})
     time_elapsed = f'{time.time() - time_init:.4f} secs'
-    print(np.argmax(onnx_pred[0][0]))
 
-    class_names = ['angry', 'happy', 'neutral', 'sad', 'surprised']
-    emotion = class_names[np.argmax(onnx_pred[0][0])]
-
-    return {'emotion': emotion,
+    return {'emotions': emotions,
             'time_elapsed_preprocessing': str(time_elapsed_preprocessing),
             'time_elapsed': str(time_elapsed)
-             }
+            }
