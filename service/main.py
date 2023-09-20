b771eab8936cde0a7be3f16bab0bfc9613150d61
rakibulhaque9954 <rakibulhaque9954@gmail.com>
2023-09-21 00:10:48 +0900

    update main.py and add html css and jss

--- service/main.py
+++ service/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from service.api.api import main_router
 import onnxruntime as rt
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 app = FastAPI(project_name='Emotion_detection')
 app.include_router(main_router)
@@ -8,8 +12,11 @@ app.include_router(main_router)
 providers = ['CPUExecutionProvider']
 model = rt.InferenceSession('service/core/logic/vit_quantized.onnx', providers=providers)
 
-@app.get('/')
-async def root():
-    return {'hello': 'world'}
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+@app.get('/', response_class=FileResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": "Rahul"})
 
 # its service.main: app
